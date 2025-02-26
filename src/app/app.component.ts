311b6b0c0eb73634bb9826232fc5a7234a8c1b40
mehranmb78 <mehranmb78@gmail.com>
2025-02-26 13:25:41 +0100

    feat: add usage of another stick element

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
+import { StickAfterScrollDirective } from './stick-after-scroll.directive/stick-after-scroll.directive';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, StickAfterScrollDirective],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-
+  isMainSectionHeaderSticky = signal(false)
 }
